c01f8204ed0ca7bfe3c5a218e1c24d78de4a7aa6
TEAM TSHAKE <nadoo.ali.15.4@gmail.com>
2018-01-09 16:29:02 -0800

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,3 +1,3 @@
--- file BY : (TshAkE TEAM)
-TshAkE_TshAkE = dofile('sudo.lua')
-os.execute('./tg -s ./TSHAKE.lua $@ —bot='..token)
+
+TshAkEl = dofile('sudo.lua')
+os.execute('./tg -s ./TSHAKE.lua $@ —bot='..TshAkEl.token)
